d9e92750b9155bc42805a5c01a854b835030c07f
farid24452 <faridislamudin340@gmil.com>
2026-02-13 10:48:34 +0700

    fix

--- lib/validators/school.ts
+++ lib/validators/school.ts
@@ -1,13 +1,14 @@
-import { EducationLevel } from "@/app/generated/prisma/enums";
 import { z } from "zod";
+
+export const EducationLevelEnum = z.enum([
+    "PRIMARY_SCHOOL",
+    "JUNIOR_HIGH_SCHOOL",
+    "SENIOR_HIGH_SCHOOL",
+]);
 
 export const createSchoolSchema = z.object({
     name: z.string().nonempty("Name is required"),
     address: z.string().nonempty("Address is required"),
     slug: z.string().nonempty("Slug is required"),
-    educationLevel: z
-        .nativeEnum(EducationLevel)
-        .refine((val) => val !== undefined && val !== null, {
-            message: "The required education level is selected",
-        }),
-})
+    educationLevel: EducationLevelEnum,
+});
